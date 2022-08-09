b30463667fcf26fe28ca9f44d3585f0c2dab8a0b
Yash Herekar <46808309+1337encrypted@users.noreply.github.com>
2022-08-09 19:47:56 +0530

    Update L298N_BLE.ino

--- L298N_BLE.ino
+++ L298N_BLE.ino
@@ -9,100 +9,99 @@ void setup(){
 
 void loop()
 {
-
+  car.Stp();
   if (Serial.available())
   {  
-    key=Serial.parseInt();
-    
-    if(key == 'F')
+    key=Serial.read();
+    case 'F':
     {
       car.front();
       Serial.println("Foreward: "+car.Speed);
     }
-    else if(key == 'B')
+    case 'B':
     {
       car.back();
       Serial.println("Backward: "+car.Speed);
     }
-    else if(key == 'L')
+    case 'L':
     {
       car.leftturn();
       Serial.println("Left: "+car.Speed);
     }
-    else if(key == 'R')
+    case 'R':
     {
       car.rightturn();
       Serial.println("Right: "+car.Speed);
     }
-    else if(key == 'I')
+    case 'I':
     {
       car.rightShift();
       Serial.println("Right Shift: "+car.Speed);
     }
-    else if(key == 'G')
+    case 'G':
     {
       car.leftShift();
       Serial.println("Left Shift: "+car.Speed);
     }
-    else if(key == '0')
+    case '0':
     {
       car.Speed = 115;
       //Serial.println("car.Speed = 115");
     }
-    else if(key == '1')
+    case '1':
     {
       car.Speed = 130;
       //Serial.println("car.Speed = 130");
     }
-    else if(key == '2')
+    case '2':
     {
       car.Speed = 143;
       //Serial.println("car.Speed = 143");
     }
-    else if(key == '3')
+    case '3':
     {
       car.Speed = 157;
       //Serial.println("car.Speed = 157");
     }
-    else if(key == '4')
+    case '4':
     {
       car.Speed = 170;
       //Serial.println("car.Speed = 170");
     }
-    else if(key == '5')
+    case '5':
     {
       car.Speed = 185;
       //Serial.println("car.Speed = 185");
     }
-    else if(key == '6')
+    case '6':
     {
       car.Speed = 200;
       //Serial.println("car.Speed = 200");
     }
-    else if(key == '7')
+    case '7':
     {
       car.Speed = 213;
       //Serial.println("car.Speed = 213");
     }
-    else if(key == '8')
+    case '8':
     {
       car.Speed = 227;
       //Serial.println("car.Speed = 227");
     }
-    else if(key == '9')
+    case '9':
     {
       car.Speed = 240;
       //Serial.println("car.Speed = 240");
     }
-    else if(key == 'q')
+    case 'q':
     {
       car.Speed = 255;
       //Serial.println("car.Speed = 255");
     }
-    else
+    default:
     {
       car.Stp();
-      Serial.println("Stop: "+0); 
+      Serial.println("stop"+Speed);
     }
   }
 }
